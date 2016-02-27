30a32d2142222379c9435af353f5d1a40840c19c
anthony <anthonygumb@gmail.com>
2016-02-27 14:50:09 +0100

    ajout du repertoire de test

---
+++ Rackfx2.0/test/controller/FicheGroupeEditControllerTest.java
@@ -0,0 +1,39 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FicheGroupeEditControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGeleTab() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGroupe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPersonne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTitre() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Rackfx2.0/test/controller/MainAppTest.java
@@ -0,0 +1,124 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainAppTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetGroupeData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRencontreData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitRootLayout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowMainView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowFicheGroupeEditDialog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowFicheEventEditDialog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrimaryStage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchClassOfQextendsApplicationStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartStage1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHostServices() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParameters() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyPreloader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+}
